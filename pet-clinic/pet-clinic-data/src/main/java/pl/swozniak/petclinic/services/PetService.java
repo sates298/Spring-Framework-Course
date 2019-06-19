f0af06be3a2651b7b312562d201e4f6cb06d69ea
sates298 <stanislaw.wo@interia.pl>
2019-06-19 13:09:02 +0200

    add services

---
+++ pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package pl.swozniak.petclinic.services;
+
+import pl.swozniak.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package pl.swozniak.petclinic.services;
+
+import pl.swozniak.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
