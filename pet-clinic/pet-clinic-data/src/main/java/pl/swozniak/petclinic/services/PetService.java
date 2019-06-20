66f6ca8ef65f77c10075b062f7714ee75b3d5c2f
sates298 <stanislaw.wo@interia.pl>
2019-06-20 16:27:40 +0200

    add crudService and inherits

--- pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/PetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/PetService.java
@@ -2,15 +2,7 @@ package pl.swozniak.petclinic.services;
 
 import pl.swozniak.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-
+public interface PetService extends CrudService<Pet, Long>{
 
 }
--- pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/VetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/pl/swozniak/petclinic/services/VetService.java
@@ -2,15 +2,8 @@ package pl.swozniak.petclinic.services;
 
 import pl.swozniak.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
     Vet findByLastName(String lastName);
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
