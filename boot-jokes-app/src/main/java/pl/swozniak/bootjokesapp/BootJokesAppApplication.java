73d25f8572ff0aac0aa0ff1f78a11b12291efaf0
sates298 <stanislaw.wo@interia.pl>
2019-06-20 15:31:25 +0200

    initial commit

---
+++ boot-jokes-app/src/main/java/pl/swozniak/bootjokesapp/BootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package pl.swozniak.bootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJokesAppApplication.class, args);
+    }
+
+}
---
+++ boot-jokes-app/src/test/java/pl/swozniak/bootjokesapp/BootJokesAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.swozniak.bootjokesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootJokesAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
