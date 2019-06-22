8629fdbb671c33c1b427d9333e83b6e238715d01
sates298 <stanislaw.wo@interia.pl>
2019-06-22 16:17:10 +0200

    add configurations

--- boot-jokes-app/src/main/java/pl/swozniak/bootjokesapp/BootJokesAppApplication.java
+++ boot-jokes-app/src/main/java/pl/swozniak/bootjokesapp/BootJokesAppApplication.java
@@ -2,8 +2,10 @@ package pl.swozniak.bootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class BootJokesAppApplication {
 
     public static void main(String[] args) {
